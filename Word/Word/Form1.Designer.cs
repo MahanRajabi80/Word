e32258c57b2e257030df5a4078f70c50259b7de6
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-01-28 12:44:40 +0330

    Add files via upload

---
+++ Word/Word/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Word
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(255, 324);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(157, 53);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Number Char, Line, Dot, Digit";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(418, 324);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(157, 53);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtEditor.Location = new System.Drawing.Point(0, 0);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(800, 290);
+            this.txtEditor.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtEditor;
+    }
+}
+
---
+++ Word/Word/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Word
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string text = txtEditor.Text;
+            int charnumber = 0, linenumber = 1, dotnumber = 0, digitnumber = 0;
+            foreach (char ch in text)
+            {
+                switch (ch)
+                {
+                    case '\n':
+                        linenumber++;
+                        break;
+                    case '.':
+                        dotnumber++;
+                        charnumber++;
+                        break;
+                    default:
+                        if (!char.IsControl(ch))
+                            charnumber++;
+                        if (char.IsControl(ch))
+                            digitnumber++;
+                        break;
+                }
+            }
+            MessageBox.Show("CharNumber=" + charnumber.ToString() + ",LineNumber=" + linenumber.ToString() + ",DotNumber=" + dotnumber.ToString() + ",DigitNumber=" + digitnumber.ToString());
+        }
+    }
+}
